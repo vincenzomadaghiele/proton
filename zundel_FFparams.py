06407bac06ff67024cdf2bbe398e4beb1d800336
vincenzo madaghiele <vmadaghiele@aegir.kl.dbb.su.se>
2023-03-13 15:20:42 +0100

    FF params

--- zundel_FFparams.py
+++ zundel_FFparams.py
@@ -1,8 +1,9 @@
 import MDAnalysis as mda
-from MDAnalysis.analysis import rms, dihedrals
+from MDAnalysis.analysis import rms
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy.optimize import leastsq
 
 
 def calculateDistance(MDAuniverse, atom_index1, atom_index2,
@@ -64,7 +65,7 @@ def calculateAngle(MDAuniverse, atom1, atom2, atom3,
         plt.xlabel("Trajectory time [fs]")
         plt.ylabel("Angle [rad]")
         plt.plot(angles[300:600], label="distance")
-        plt.axhline(theta0, color='r', linestyle='dashed', label="r0")
+        plt.axhline(theta0, color='r', linestyle='dashed', label="theta0")
         plt.legend()
         plt.show()
     
@@ -74,7 +75,7 @@ def calculateAngle(MDAuniverse, atom1, atom2, atom3,
         plt.xlabel("Angle [rad]")
         plt.ylabel("Density")
         sns.kdeplot(data=angles, label="distance")
-        plt.axvline(theta0, color='r', linestyle='dashed', label="r0")
+        plt.axvline(theta0, color='r', linestyle='dashed', label="theta0")
         plt.legend()
         plt.show()
     
@@ -108,7 +109,7 @@ def calculateImproper(MDAuniverse, atom1, atom2, atom3, atom4,
         plt.xlabel("Trajectory time [fs]")
         plt.ylabel("Angle [rad]")
         plt.plot(impropers[300:600], label="distance")
-        plt.axhline(xi0, color='r', linestyle='dashed', label="r0")
+        plt.axhline(xi0, color='r', linestyle='dashed', label="xi0")
         plt.legend()
         plt.show()
     
@@ -118,7 +119,7 @@ def calculateImproper(MDAuniverse, atom1, atom2, atom3, atom4,
         plt.xlabel("Angle [rad]")
         plt.ylabel("Density")
         sns.kdeplot(data=impropers, label="distance")
-        plt.axvline(xi0, color='r', linestyle='dashed', label="r0")
+        plt.axvline(xi0, color='r', linestyle='dashed', label="xi0")
         plt.legend()
         plt.show()
     
@@ -134,6 +135,66 @@ def calculateImproper(MDAuniverse, atom1, atom2, atom3, atom4,
         plt.show()
 
     return xi0, k_xi, impropers
+
+def calculateDihedral(MDAuniverse, atom1, atom2, atom3, atom4,
+                      printExample=False, printHist=False, printPotential=False, atom_names=""):
+    
+    dihedrals = []
+    for ts in pto.trajectory:
+        dihedral = pto.atoms[[atom1, atom2, atom3, atom4]].dihedral.value() # group three atoms
+        dihedrals.append(dihedral)
+    dihedrals = np.array(dihedrals) * (np.pi/180) # convert to radians
+    
+    if printExample:
+        # distances over time
+        plt.title(f"Dihedral {atom_names}")
+        plt.xlabel("Trajectory time [fs]")
+        plt.ylabel("Angle [rad]")
+        plt.plot(dihedrals[300:600], label="distance")
+        plt.legend()
+        plt.show()
+    
+    if printHist:
+        # histogram of distances
+        plt.title(f"Histogram of dihedral {atom_names}")
+        plt.xlabel("Angle [rad]")
+        plt.ylabel("Density")
+        sns.kdeplot(data=dihedrals, label="distance")
+        plt.legend()
+        plt.show()
+
+    # calculate p(r)
+    counts, bins = np.histogram(dihedrals, bins=100, density=True)    
+    energies = - 0.616 * np.log(counts) # convert p(r) to energy E(i)
+    N = counts.shape[0] # number of data points
+    t = np.linspace(0, 2*np.pi, N)
+    data = energies - np.min(energies)
+            
+    # fit cosine to energy 
+    guess_phase = 1
+    guess_amp = 1
+    optimize_func = lambda x: x[0] * (1 + np.cos(2* t - x[1])) - data
+    k_phi, phi0 = leastsq(optimize_func, [guess_amp, guess_phase])[0]
+    
+    # recreate the fitted curve using the optimized parameters
+    data_fit = k_phi * (1 + np.cos(2 * t + phi0))
+    
+    # recreate the fitted curve using the optimized parameters
+    fine_t = np.arange(0,max(t),0.1)
+    data_fit = k_phi * (1 + np.cos(2 * fine_t + phi0))
+    
+    if printPotential:
+    
+        plt.title("Potential of dihedral angle of H4-O1-O0-H2")
+        plt.xlabel("Angle [rad]")
+        plt.ylabel("Density")
+        plt.plot(t, data, '.')
+        plt.plot(fine_t, data_fit, label='fitted curve')
+        plt.legend()
+        plt.show()
+
+    return k_phi, phi0, dihedrals
+
 
 
 if __name__ == '__main__':
@@ -169,6 +230,7 @@ if __name__ == '__main__':
     print('Bond distance parameters')
     print('-'*20)
     
+    # specify list of bonds
     bonds = [[2, 0], [6, 0], [3, 0], [3, 1], [4, 1], [5, 1]]
     r_b = []
     k_b = []
@@ -189,6 +251,7 @@ if __name__ == '__main__':
     print('Angle parameters')
     print('-'*20)
     
+    # specify list of angles
     angles = [[2, 0, 6], [6, 0, 3], [2, 0, 3], 
               [4, 1, 3], [4, 1, 5], [5, 1, 3], [0, 3, 1]]
     theta0 = []
@@ -210,6 +273,7 @@ if __name__ == '__main__':
     print('Impropers parameters')
     print('-'*20)
     
+    # specify list of improper dihedrals
     impropers = [[2, 6, 3, 0], [4, 5, 3, 1]]
     xi0 = []
     k_xi = []
@@ -221,4 +285,25 @@ if __name__ == '__main__':
         print(atom_names)
         print(f'k_xi: {k}')
         print(f'xi0: {xi}')
+        print()
+
+
+    #%% Dihedral angles
+    
+    print()
+    print('Dihedral angle parameters')
+    print('-'*20)
+    
+    # specify list of improper dihedrals
+    dihedrals = [[2, 0, 1, 4], [2, 0, 1, 5]]
+    k_phis = []
+    phi0s = []
+    for dihedral in dihedrals:
+        atom_names = f'{atoms.names[dihedral[0]]}{dihedral[0]}-{atoms.names[dihedral[1]]}{dihedral[1]}-{atoms.names[dihedral[2]]}{dihedral[2]}-{atoms.names[dihedral[3]]}{dihedral[3]}'
+        k_phi, phi0, _ = calculateDihedral(pto, dihedral[0], dihedral[1], dihedral[2], dihedral[3], True, True, True, atom_names)
+        k_phis.append(k_phi)
+        phi0s.append(phi0)
+        print(atom_names)
+        print(f'k_phi: {k_phi}')
+        print(f'phi0: {phi0}')
         print()
